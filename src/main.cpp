a20a9aa6101a24b657dc3b80b498ee721d617bc6
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-04-22 16:48:27 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,22 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/EditorUI.hpp>
+
+using namespace geode::prelude;
+
+class $modify(EditorUI) {
+    bool init(LevelEditorLayer* editorLayer) {
+        if (!EditorUI::init(editorLayer)) return false;
+
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+        auto label = CCLabelBMFont::create("EditorUI", "chatFont.fnt");
+        label->setScale(0.5f);
+        label->setOpacity(127); // 0.5 opacity (255 x 0.5)
+        label->setAnchorPoint({ 0.f, 1.f });
+        label->setPosition({ 5.f, winSize.height - 5.f });
+        label->setZOrder(999);
+        this->addChild(label);
+
+        return true;
+    }
+};
